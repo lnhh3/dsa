6fa1b630c0c2f362be23ea6fa3dc29d3d0daeaa9
HuyLeNguyenHoang <lhuy100000@gmail.com>
2025-10-09 16:51:48 +0700

    2 pointers solution

--- linked-lists/src/Main.java
+++ linked-lists/src/Main.java
@@ -1,16 +1,28 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        SinglyLinkedList<String> list = new SinglyLinkedList<>();
-        list.insertTail("1");
-        list.insertTail("2");
-        list.insertTail("3");
-        list.insertTail("4");
-        list.insertTail("5");
-        list.insertTail("6");
-        list.insertTail("7");
+//        int[] arr = {1, 2, 3, 4};
+        String s = "Was it a car or a cat I saw?";
+//        String t = "carrace";
 
-        list.remove(3);
-        list.insertHead("0");
-        System.out.println(list.getValues());
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int l = 0;
+        s = s.replaceAll(" ", "").toLowerCase();
+
+        for (int r = s.length() - 1; r >= s.length() / 2; r--, l++) {
+            if (isASCII(s.charAt(r))) --r;
+            if (isASCII(s.charAt(l))) ++l;
+            if (s.charAt(l) != s.charAt(r)) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isASCII(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
     }
 }
